a8ec59b92049977c412d6306796ef9e1a61de448
Oscar Henry Torres Caballero <oscar.torres@lighthousetech.io>
2023-05-22 16:50:02 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sendbird_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sendbird_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/channels_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChannelScreen extends StatefulWidget {
+  const ChannelScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChannelScreen> createState() => _ChannelScreenState();
+}
+
+class _ChannelScreenState extends State<ChannelScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
